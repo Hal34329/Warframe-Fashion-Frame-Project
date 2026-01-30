82ba824a86ae202a0ed79cd260d3afed0c1d529e
Hal34329 <jesusguillen545@gmail.com>
2026-01-29 20:44:47 -0600

    SCP-5000 Why?

--- packages/config/eslint/react.js
+++ packages/config/eslint/react.js
@@ -1,21 +1,21 @@
-import { baseConfig } from "./base.js";
-import reactPlugin from "eslint-plugin-react";
-import hooksPlugin from "eslint-plugin-react-hooks";
+// import { baseConfig } from "./base.js";
+// import reactPlugin from "eslint-plugin-react";
+// import hooksPlugin from "eslint-plugin-react-hooks";
 
-export const reactConfig = [
-    ...baseConfig,
-    {
-        plugins: {
-            react: reactPlugin,
-            "react-hooks": hooksPlugin,
-        },
-        rules: {
-            ...reactPlugin.configs.recommended.rules,
-            ...hooksPlugin.configs.recommended.rules,
-            "react/react-in-jsx-scope": "off",
-        },
-        settings: {
-            react: { version: "detect" },
-        },
-    }
-];
+// export const reactConfig = [
+//     ...baseConfig,
+//     {
+//         plugins: {
+//             react: reactPlugin,
+//             "react-hooks": hooksPlugin,
+//         },
+//         rules: {
+//             ...reactPlugin.configs.recommended.rules,
+//             ...hooksPlugin.configs.recommended.rules,
+//             "react/react-in-jsx-scope": "off",
+//         },
+//         settings: {
+//             react: { version: "detect" },
+//         },
+//     }
+// ];
