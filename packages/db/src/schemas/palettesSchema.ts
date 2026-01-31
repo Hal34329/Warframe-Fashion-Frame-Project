398095cf44dabf3c3ab365f4dac738d04f1f40f8
Hal34329 <jesusguillen545@gmail.com>
2026-01-31 15:44:02 -0600

    Add manual ID to palettes

--- packages/db/src/schemas/palettesSchema.ts
+++ packages/db/src/schemas/palettesSchema.ts
@@ -1,9 +1,9 @@
-import { pgTable, serial, varchar, jsonb } from "drizzle-orm/pg-core";
+import { pgTable, integer, varchar, jsonb } from "drizzle-orm/pg-core";
 
 export type ColorPalette = Record<`color_${number}`, string>;
 
 export const palettes = pgTable("palettes", {
-    id: serial("id").primaryKey(),
+    id: integer("id").primaryKey(),
     name: varchar("name", { length: 50 }).notNull().unique(),
     colors: jsonb("colors").$type<ColorPalette>().notNull(),
 });
